8e9176fc24634891b26fa705a3116d1e760944cc
llamersmcw <llamers@mcw.edu>
2025-03-31 13:34:26 -0500

    more ram

--- src/main/dist/runSampleQc.sh
+++ src/main/dist/runSampleQc.sh
@@ -13,6 +13,6 @@ cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
-    -jar lib/$APPNAME.jar -sampleQc "$@" > run.log 2>&1
+    -Xmx20g -jar lib/$APPNAME.jar -sampleQc "$@" > run.log 2>&1
 
 mailx -s "[$SERVER] Variant QC Pipeline" $EMAILLIST < $APPDIR/logs/summary.log
